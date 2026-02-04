e32c9a991eb553c7a1f3c1aa285f7555271d2c23
Julioj1 <dbjulioj@gmail.com>
2026-02-04 12:34:21 -0500

    Made advances to the from-Scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,10 +1,18 @@
 const resolvedWrapper = (value) => {
+  return new Promise((resolve, reject) => {
+    resolve(value);
+  })
 };
 
 const rejectedWrapper = (errorMessage) => {
+  return Promise.reject(new Error (errorMessage));
 };
 
 const handleResolvedPromise = (promise) => {
+  return promise.then((message) => {
+    console.log(message);
+    return message.toUpperCase();
+  })
 };
 
 const handleResolvedOrRejectedPromise = (promise) => {
