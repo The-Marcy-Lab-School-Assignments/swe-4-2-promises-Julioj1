c9b0d1dd211dcd0035e1808c980fe059dfa99837
Julioj1 <dbjulioj@gmail.com>
2026-02-05 19:34:28 -0500

    Completed from-scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -16,9 +16,23 @@ const handleResolvedPromise = (promise) => {
 };
 
 const handleResolvedOrRejectedPromise = (promise) => {
+  return promise
+    .then((message) => {
+    console.log(message);
+    return message.toUpperCase();
+  })
+    .catch((error) => {
+      console.error(`Your error message was: ${error.message}`);
+      return null;
+    })
 };
 
 const pauseForMs = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>{
+      resolve(undefined);
+    }, ms)
+  })
 };
 
 module.exports = {
