d7395b91c7563d6ae418f3463e19f2ce5ad3bef9
Julioj1 <dbjulioj@gmail.com>
2026-02-05 23:50:51 -0500

    Completed modify.js

--- src/modify.js
+++ src/modify.js
@@ -12,6 +12,7 @@ Notice how the code keeps indenting further and further right.
 This is hard to read, hard to debug, and hard to maintain.
 */
 const fsCallback = require('fs');
+const { promiseHooks } = require('v8');
 const getStoryCallbackHell = (callback) => {
   fsCallback.readFile(getPath('story-part-1.txt'), 'utf-8', (err, part1) => {
     if (err) { console.error(err); return; }
@@ -41,12 +42,20 @@ const readFileSequentially = () => {
   const storyParts = [];
   return fs.readFile(getPath('story-part-1.txt'), 'utf-8')
     .then((part1) => {
+      storyParts.push(part1);
+      return fs.readFile(getPath('story-part-2.txt'), 'utf-8');
     })
     .then((part2) => {
+      storyParts.push(part2);
+      return fs.readFile(getPath('story-part-3.txt'), 'utf-8');
     })
     .then((part3) => {
+      storyParts.push(part3);
+      return fs.readFile(getPath('story-part-4.txt'), 'utf-8');
     })
     .then((part4) => {
+      storyParts.push(part4);
+      return storyParts.join(`\n`);
     })
     .catch((err) => {
       console.error(err);
@@ -60,6 +69,16 @@ const readFileSequentially = () => {
 // - Use .then() to join the parts after Promise.all resolves
 // - Don't forget .catch() for error handling!
 const readFilesParallel = () => {
+  return Promise.all([
+    fs.readFile(getPath('story-part-1.txt'), 'utf-8'),
+    fs.readFile(getPath('story-part-2.txt'), 'utf-8'),
+    fs.readFile(getPath('story-part-3.txt'), 'utf-8'),
+    fs.readFile(getPath('story-part-4.txt'), 'utf-8')
+  ])
+  .then((storyParts) => {
+    return storyParts.join('\n');
+  })
+  .catch(console.error);
 };
 
 module.exports = {
